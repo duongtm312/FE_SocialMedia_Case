7b867182645e5abde329408704d0bb5a9ac0ed02
Conganh97 <anhbc.afsystem@gmail.com>
2022-08-05 08:35:31 +0700

    homeprofile

--- assets/jsPost/posthome.js
+++ assets/jsPost/posthome.js
@@ -1,6 +1,8 @@
 
 
 let token = localStorage.getItem("token");
+postHome();
+showProfile()
 
 function postHome (){
     $.ajax({
@@ -15,12 +17,13 @@ function postHome (){
             console.log(token)
             xhr.setRequestHeader ("Authorization", "Bearer " + token);
         },
-        url: "http://localhost:8081/home",
+        url: "http://localhost:8081/home/post",
         // data: JSON.stringify(),
         //xử lý khi thành công
         success: function (data) {
             console.log("thành công")
             console.log(data)
+            // showProfile()
             show(data);
         },
         error: function (err) {
@@ -28,15 +31,6 @@ function postHome (){
         }
     })
 }
-// function parseJwt (token) {
-//     var base64Url = token.split('.')[1];
-//     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-//     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-//         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-//     }).join(''));
-//
-//     return JSON.parse(jsonPayload);
-// };
 
 function show(data) {
     console.log("show")
@@ -53,7 +47,7 @@ function show(data) {
                 <div>
                   <div class="nav nav-divider">
                     <h6 class="nav-item card-title mb-0"> <a href="#!"> ${p.appUser.userName} </a></h6>
-                    <span class="nav-item small"> ${p.timePost}</span>
+                    <span class="nav-item small" > ${p.timePost}</span>
                   </div>
                 </div>
               </div>
@@ -202,4 +196,140 @@ function show(data) {
     }
     document.getElementById("showhome").innerHTML = str;
 }
-window.onload(postHome())
+function showProfile(){
+    $.ajax({
+        type: "GET",
+        headers: {
+            //kiểu dữ liệu nhận về
+            'Accept': 'application/json',
+            // kiểu truyền đi
+            // 'Content-Type': 'application/json'
+        },
+        beforeSend: function (xhr) {
+            console.log(token)
+            xhr.setRequestHeader ("Authorization", "Bearer " + token);
+        },
+        url: "http://localhost:8081/home/profile",
+        // data: JSON.stringify(),
+        //xử lý khi thành công
+        success: function (dataProfile) {
+            console.log("thành công")
+            console.log(dataProfile)
+            showDateProfile(dataProfile);
+        },
+        error: function (err) {
+            console.log("lỗi")
+        }
+    })
+}
+
+function showDateProfile(dataProfile){
+    let str = `<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasSideNavbar">
+            <!-- Offcanvas header -->
+            <div class="offcanvas-header">
+              <button type="button" class="btn-close text-reset ms-auto" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+
+            <!-- Offcanvas body -->
+            <div class="offcanvas-body d-block px-2 px-lg-0">
+              <!-- Card START -->
+              <div class="card overflow-hidden">
+                <!-- Cover image -->
+                <div class="h-50px" style="background-image:url(${dataProfile.photoCoverSrc}); background-position: center; background-size: cover; background-repeat: no-repeat;"></div>
+                  <!-- Card body START -->
+                  <div class="card-body pt-0">
+                    <div class="text-center">
+                    <!-- Avatar -->
+                    <div class="avatar avatar-lg mt-n5 mb-3">
+                      <a href="#!"><img class="avatar-img rounded border border-white border-3" src="${dataProfile.avatarSrc}" alt=""></a>
+                    </div>
+                    <!-- Info -->
+                    <h5 class="mb-0"> <a href="#!">${dataProfile.fullName} </a> </h5>
+                    <small>C0322G1</small>
+                    <p class="mt-3">Bug nhiều chán qá rồi</p>
+
+                    <!-- User stat START -->
+                    <div class="hstack gap-2 gap-xl-3 justify-content-center">
+                      <!-- User stat item -->
+                      <div>
+                        <h6 class="mb-0">256</h6>
+                        <small>Post</small>
+                      </div>
+                      <!-- Divider -->
+                      <div class="vr"></div>
+                      <!-- User stat item -->
+                      <div>
+                        <h6 class="mb-0">2.5K</h6>
+                        <small>Friend</small>
+                      </div>
+
+                    </div>
+                    <!-- User stat END -->
+                  </div>
+
+                  <!-- Divider -->
+                  <hr>
+
+                  <!-- Side Nav START -->
+                  <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
+                    <li class="nav-item">
+                      <a class="nav-link" href="my-profile.html"> <img class="me-2 h-20px fa-fw" src="assets/images/icon/home-outline-filled.svg" alt=""><span>Feed </span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="my-profile-connections.html"> <img class="me-2 h-20px fa-fw" src="assets/images/icon/person-outline-filled.svg" alt=""><span>Connections </span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="blog.html"> <img class="me-2 h-20px fa-fw" src="assets/images/icon/earth-outline-filled.svg" alt=""><span>Latest News </span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="events.html"> <img class="me-2 h-20px fa-fw" src="assets/images/icon/calendar-outline-filled.svg" alt=""><span>Events </span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="groups.html"> <img class="me-2 h-20px fa-fw" src="assets/images/icon/chat-outline-filled.svg" alt=""><span>Groups </span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="notifications.html"> <img class="me-2 h-20px fa-fw" src="assets/images/icon/notification-outlined-filled.svg" alt=""><span>Notifications </span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="settings.html"> <img class="me-2 h-20px fa-fw" src="assets/images/icon/cog-outline-filled.svg" alt=""><span>Settings </span></a>
+                    </li>
+                  </ul>
+                  <!-- Side Nav END -->
+                </div>
+                <!-- Card body END -->
+                <!-- Card footer -->
+                <div class="card-footer text-center py-2">
+                  <a class="btn btn-link btn-sm" href="my-profile.html">View Profile </a>
+                </div>
+              </div>
+              <!-- Card END -->
+
+              <!-- Helper link START -->
+              <ul class="nav small mt-4 justify-content-center lh-1">
+                <li class="nav-item">
+                  <a class="nav-link" href="my-profile-about.html">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="settings.html">Settings</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link"  href="http://www.bootstrapmb.comlogin">Support </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link"  href="">Docs </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="help.html">Help</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="privacy-and-terms.html">Privacy & terms</a>
+                </li>
+              </ul>
+              <!-- Helper link END -->
+              <!-- Copyright -->
+              <p class="small text-center mt-1">©2022 <a class="text-body"  href="http://www.bootstrapmb.com/"> Webestica </a></p>
+            </div>
+          </div>`
+
+    document.getElementById("profile").innerHTML = str;
+}
