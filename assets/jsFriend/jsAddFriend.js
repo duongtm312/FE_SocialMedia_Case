438bb6f65ace3cb8e6ab19c88598ffa787f1b0a2
Conganh97 <anhbc.afsystem@gmail.com>
2022-08-07 15:49:38 +0700

    create cmt

--- assets/jsFriend/jsAddFriend.js
+++ assets/jsFriend/jsAddFriend.js
@@ -1,46 +1,46 @@
-function acceptFriend(user){
-    $.ajax({
-        type: "GET",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader("Authorization", "Bearer " + token);
-        },
-        data: {
-            user: user,
-        },
-        url: "http://localhost:8081/acceptFriend",
-        //xử lý khi thành công
-        success: function (data) {
-        },
-        error: function (err) {
-            console.log(err)
-        }
-
-    })
-}
-function deleteAdd(user){
-    $.ajax({
-        type: "GET",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader("Authorization", "Bearer " + token);
-        },
-        data: {
-            user: user,
-        },
-        url: "http://localhost:8081/deleteAdd",
-        //xử lý khi thành công
-        success: function (data) {
-        },
-        error: function (err) {
-            console.log(err)
-        }
-
-    })
-}
+// function acceptFriend(user){
+//     $.ajax({
+//         type: "GET",
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         beforeSend: function (xhr) {
+//             xhr.setRequestHeader("Authorization", "Bearer " + token);
+//         },
+//         data: {
+//             user: user,
+//         },
+//         url: "http://localhost:8081/acceptFriend",
+//         //xử lý khi thành công
+//         success: function (data) {
+//         },
+//         error: function (err) {
+//             console.log(err)
+//         }
+//
+//     })
+// }
+// function deleteAdd(user){
+//     $.ajax({
+//         type: "GET",
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         beforeSend: function (xhr) {
+//             xhr.setRequestHeader("Authorization", "Bearer " + token);
+//         },
+//         data: {
+//             user: user,
+//         },
+//         url: "http://localhost:8081/deleteAdd",
+//         //xử lý khi thành công
+//         success: function (data) {
+//         },
+//         error: function (err) {
+//             console.log(err)
+//         }
+//
+//     })
+// }
