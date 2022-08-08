44fde0ca996121266a8661890975ddb72c60d8c0
CodeXplore <thuhoi0205@gmail.com>
2022-08-08 13:47:22 +0700

    no change

--- ajax/register.js
+++ ajax/register.js
@@ -11,12 +11,7 @@ function register() {
             passWord: passWord,
             confirmPassword: confirmPassword
         };
-    let sameName = "This username is already exist, please try again!";
-    let incorrectPassword = "Confirm password not match, please try again!";
-    let createSuccess = "Create success!";
-    let no_field = "Please enter all the fields!";
-    let no_email = "This email is already exist";
-    let not_an_email = "Not an email, try again!"
+
 
     $.ajax({
         type: "POST",
@@ -29,7 +24,7 @@ function register() {
         url: "http://localhost:8081/register",
         data: JSON.stringify(user),
             success: function (data) {
-                document.getElementById("register-err").innerHTML =`  <p    class="small-font text-uppercase text-center py-2 text-success bg-success-light2 " >Create account success !</p>`
+            document.getElementById("register-err").innerHTML =`  <p    class="small-font text-uppercase text-center py-2 text-success bg-success-light2 " >Create account success !</p>`
             },
             error(){
                 document.getElementById("register-err").innerHTML =`  <p style="filter: brightness(120%)" id="messageFailed" class="small-font text-uppercase text-center py-2 text-danger bg-danger-light2 ">"Create account fail !</p>`
