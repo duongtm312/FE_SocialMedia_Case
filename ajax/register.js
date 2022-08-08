d6a54d8a571d13e37cc3775c9c3e2f7f3c837e3a
CodeXplore <thuhoi0205@gmail.com>
2022-08-08 15:38:36 +0700

    avatar

--- ajax/register.js
+++ ajax/register.js
@@ -24,7 +24,14 @@ function register() {
         url: "http://localhost:8081/register",
         data: JSON.stringify(user),
             success: function (data) {
-            document.getElementById("register-err").innerHTML =`  <p    class="small-font text-uppercase text-center py-2 text-success bg-success-light2 " >Create account success !</p>`
+
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: 'Add Friend Success',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
             },
             error(){
                 document.getElementById("register-err").innerHTML =`  <p style="filter: brightness(120%)" id="messageFailed" class="small-font text-uppercase text-center py-2 text-danger bg-danger-light2 ">"Create account fail !</p>`
@@ -32,15 +39,5 @@ function register() {
         })
     }
 
-// function message() {
-//
-//     Swal.fire({
-//         position: 'top-end',
-//         icon: 'success',
-//         title: 'Your work has been saved',
-//         showConfirmButton: false,
-//         timer: 1500
-//     })
-// }
 
 
